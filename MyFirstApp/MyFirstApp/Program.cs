6b6a0ede93c1b492dc88c9d85128daa201795d34
Savas Tomasz Seckiner <s27508@pjwstk.edu.pl>
2024-03-08 18:47:00 +0100

    Get Average Method

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("It's 18:27");
 Console.WriteLine("It's 18:28");
 Console.WriteLine("It's 18:29");
+
+static double GetAverage(int[] numbers)
+{
+    double sum = 0.0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return sum / numbers.Length;
+}
